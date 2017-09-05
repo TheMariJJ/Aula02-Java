2ddd4b0f7873b8a5661e56e7b8dc6e768f225113
Mari San'Lima <maridelima97@gmail.com>
2017-09-05 19:36:24 -0300

    Minha primeira aplicação publicada no gitHub

---
+++ src/aula/SegundoPrograma.java
@@ -0,0 +1,11 @@
+package aula;
+
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Teste de publicacao no github
+		System.out.println("publicando o primeiro programa no gitHub");
+	}
+
+}
